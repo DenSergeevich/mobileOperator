fb6e9c3566f2990841e232a2190aff584357c867
Denis <108453494+DenSergeevich@users.noreply.github.com>
2022-10-28 01:20:00 +0500

    Modified 2
    Adding bonus in balance

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@ public class Main {
     public static void main(String[] args) {
         int initialBalance = 150; // Начальный баланс
         int refillAmount = 1250; // Сумма пополнения
-        int balance = initialBalance +  refillAmount;
+        int bonus = refillAmount > 1000 ? (refillAmount/100) : 0;
+        int balance = initialBalance +  refillAmount + bonus;
         System.out.println("Итоговый счет = " + balance);
 
-        int bonus = refillAmount > 1000 ? (refillAmount/100) : 0;
+        
         System.out.println("Бонус = " + bonus );
     }
 }
